b14079d814b9dbc7c49b954da7c9ba2f2dcf957a
Natsu Kagami <natsukagami@gmail.com>
2020-09-07 21:16:33 -0400

    Update tests

--- src/test.rs
+++ src/test.rs
@@ -1,37 +1,43 @@
 use super::*;
 
-#[test]
-pub fn user_works() {
-    let http = reqwest::blocking::Client::new();
+#[tokio::test]
+pub async fn user_works() {
+    let http = reqwest::Client::new();
+    let http = Client::new(http);
 
     // user.info
-    User::info(&http, &["natsukagami", "vjudge2"]).unwrap();
+    User::info(&http, &["natsukagami", "vjudge2"])
+        .await
+        .unwrap();
     // user.rating
-    User::rating(&http, "natsukagami").unwrap();
+    User::rating(&http, "natsukagami").await.unwrap();
     // user.status
-    User::status(&http, "natsukagami", 0, 1000).unwrap();
+    User::status(&http, "natsukagami", 0, 1000).await.unwrap();
 }
 
-#[test]
-pub fn user_listing_works() {
-    let http = reqwest::blocking::Client::new();
+#[tokio::test]
+pub async fn user_listing_works() {
+    let http = reqwest::Client::new();
+    let http = Client::new(http);
 
     // user.rated_list
-    User::rated_list(&http, false).unwrap();
+    User::rated_list(&http, false).await.unwrap();
 }
 
-#[test]
-pub fn contest_listing_works() {
-    let http = reqwest::blocking::Client::new();
+#[tokio::test]
+pub async fn contest_listing_works() {
+    let http = reqwest::Client::new();
+    let http = Client::new(http);
 
     // contest.list
-    Contest::list(&http, true).unwrap();
+    Contest::list(&http, true).await.unwrap();
 }
 
-#[test]
-pub fn contest_works() {
-    let http = reqwest::blocking::Client::new();
+#[tokio::test]
+pub async fn contest_works() {
+    let http = reqwest::Client::new();
+    let http = Client::new(http);
 
     // contest.standings
-    Contest::standings(&http, 566, |f| f).unwrap();
+    Contest::standings(&http, 566, |f| f).await.unwrap();
 }
