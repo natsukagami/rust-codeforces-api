68f3f7264e175605ebc560e57f0a644c8037694d
Natsu Kagami <nki@nkagami.me>
2021-02-18 03:26:39 +0900

    Change ratelimit impl

--- src/client.rs
+++ src/client.rs
@@ -1,15 +1,15 @@
 use reqwest::Client as HTTP;
 /// Client represents a Codeforces API client.
 /// It wraps around a reqwest HTTP client and provides rate-limiting.
-pub struct Client(rate_limit::RateLimit<HTTP>);
+pub struct Client(rate_limit::Ratelimit<HTTP>);
 
 // Number of requests per second to be rate-limited.
-pub const REQUESTS_PER_SECOND: u64 = 4;
+pub const REQUESTS_PER_SECOND: usize = 4;
 
 impl Client {
     /// New creates a new Client.
     pub fn new() -> Self {
-        Self(rate_limit::RateLimit::new(
+        Self(rate_limit::Ratelimit::new(
             HTTP::new(),
             REQUESTS_PER_SECOND,
             std::time::Duration::from_secs(1),
@@ -17,53 +17,76 @@ impl Client {
     }
 
     /// Borrows and returns the inner HTTP client.
-    pub(crate) async fn borrow(&self) -> &HTTP {
+    pub(crate) async fn borrow<'a>(&'a self) -> impl std::ops::Deref<Target = HTTP> + 'a {
         self.0.borrow().await
     }
 }
 
 mod rate_limit {
-    use futures_util::lock::Mutex;
-    use std::time::{Duration, Instant};
-    pub struct RateLimit<T> {
-        item: T,
-        clock: Mutex<(u64, Instant)>,
-        lock_count: u64,
-        lock_duration: Duration,
+    /// Provides a simple ratelimit lock (that only works in tokio)
+    // use tokio::time::
+    use std::time::Duration;
+
+    use flume::{bounded as channel, Receiver, Sender};
+    use std::ops::Deref;
+
+    /// Holds the underlying `T` in a rate-limited way.
+    pub struct Ratelimit<T> {
+        inner: T,
+        recv: Receiver<()>,
+        send: Sender<()>,
+
+        wait_time: Duration,
     }
 
-    impl<T: 'static> RateLimit<T> {
-        pub fn new(item: T, count: u64, duration: Duration) -> Self
-        where
-            T: 'static,
-        {
+    struct RatelimitGuard<'a, T> {
+        inner: &'a T,
+        send: &'a Sender<()>,
+        wait_time: &'a Duration,
+    }
+
+    impl<T> Ratelimit<T> {
+        /// Create a new ratelimit with at most `count` uses in `wait_time`.
+        pub fn new(inner: T, count: usize, wait_time: Duration) -> Self {
+            let (send, recv) = channel(count);
+            (0..count).for_each(|_| {
+                send.send(()).ok();
+            });
             Self {
-                item,
-                clock: Mutex::new((count, Instant::now() + duration)),
-                lock_count: count,
-                lock_duration: duration,
+                inner,
+                send,
+                recv,
+                wait_time,
             }
         }
 
-        pub async fn borrow(&self) -> &T {
-            self.wait_for_clock().await;
-            &self.item
-        }
-
-        // pub async fn borrow_mut(&mut self) -> &mut T {
-        //     self.wait_for_clock().await;
-        //     &mut self.item
-        // }
-
-        async fn wait_for_clock(&self) {
-            let mut clock = self.clock.lock().await;
-            let (times_left, next_clock) = *clock;
-            if times_left == 0 {
-                tokio::time::sleep_until(next_clock.into()).await;
-                *clock = (self.lock_count, Instant::now() + self.lock_duration);
-            } else {
-                *clock = (times_left - 1, next_clock);
+        /// Borrow the inner `T`. You can only hol this reference `count` times in `wait_time`.
+        /// The clock counts from the moment the ref is dropped.
+        pub async fn borrow<'a>(&'a self) -> impl Deref<Target = T> + 'a {
+            self.recv.recv_async().await.unwrap();
+            RatelimitGuard {
+                inner: &self.inner,
+                send: &self.send,
+                wait_time: &self.wait_time,
             }
+        }
+    }
+
+    impl<'a, T> Deref for RatelimitGuard<'a, T> {
+        type Target = T;
+        fn deref(&self) -> &Self::Target {
+            self.inner
+        }
+    }
+
+    impl<'a, T> Drop for RatelimitGuard<'a, T> {
+        fn drop(&mut self) {
+            let send = self.send.clone();
+            let wait_time = self.wait_time.clone();
+            tokio::spawn(async move {
+                tokio::time::sleep(wait_time).await;
+                send.send_async(()).await.ok();
+            });
         }
     }
 }
